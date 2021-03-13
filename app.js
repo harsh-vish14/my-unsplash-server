38cb3e1722edc8671c53c211f692555da6b4ea6d
harsh-vish14 <harshkv9322@gmail.com>
2021-03-13 22:35:03 +0530

    removing bugs

--- app.js
+++ app.js
@@ -16,15 +16,12 @@ firebase.initializeApp(config)
 // const ref = firebase.database().ref();
 
 const app = express()
-app.use(function (req, res, next) {
-
-    res.setHeader('Access-Control-Allow-Origin', 'https://peaceful-eyrie-75408.herokuapp.com/');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
